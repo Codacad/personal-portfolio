3d477d7077eab36ead4255df4e7339d7a125f581
Mohd Rizwan <iamrizwan40@gmail.com>
2024-03-11 14:32:22 +0300

    added the resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,14 +1,77 @@
-import React from 'react'
+import React from "react";
 
 const Resume = () => {
   return (
     <>
-      <div className="bg-white resume relative">
-        <h1>Resume</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quaerat deserunt commodi beatae perferendis illum esse ex sapiente accusamus consequatur, nesciunt culpa ab deleniti fuga incidunt repellendus suscipit nemo numquam iste harum voluptatibus officiis. Maxime ad nesciunt, quasi esse temporibus aut eligendi, facilis, quam tempora ea mollitia voluptatum quas delectus neque id. Vero ducimus dolore accusantium nostrum perferendis similique numquam natus laudantium? Voluptate autem explicabo debitis soluta placeat magni minima voluptas consequatur mollitia in, et doloremque, hic, libero quisquam delectus provident impedit quis similique obcaecati magnam? Totam, illum architecto! Ipsum, atque laborum! Laboriosam nemo excepturi esse iure unde illo aliquam molestias? Adipisci amet accusantium quidem iste voluptate, consequatur itaque eum unde commodi facilis, cumque nihil quia quam quasi? Blanditiis eius quisquam, quaerat necessitatibus vero ipsa. Libero, accusamus obcaecati incidunt quos architecto at totam aliquam aperiam consequuntur! Dolores odit est excepturi earum veritatis temporibus possimus sequi reprehenderit amet iusto, ipsam similique odio officia quibusdam expedita enim. Quos quas possimus eveniet beatae odio cupiditate molestiae ipsa error hic rerum voluptatum delectus doloremque odit nemo mollitia id, sunt ipsam sed voluptatibus nihil. Quisquam voluptates temporibus voluptatum error ipsam. Doloribus consequatur voluptate in quasi tempore fuga odio officiis non quae quam magnam architecto ratione sed rem, cumque dolor dolorum praesentium est at delectus magni temporibus eum! Officia quam nihil accusantium corrupti, unde natus. Dolorum consequuntur repudiandae, voluptatibus earum modi vitae facilis ducimus incidunt quisquam consectetur nisi iure sed natus quibusdam ipsum. Nam sequi autem blanditiis corrupti dolore nihil voluptatum deleniti quidem eius beatae ipsum quisquam rem nesciunt aut, dicta ex praesentium, excepturi, sint nisi magnam. Amet porro quia quo minima ipsam qui eligendi quos fugit repudiandae aliquid culpa inventore, cum eos commodi earum, nemo beatae voluptatum a rem ad, eaque debitis error sint optio? Rerum voluptate animi minima sequi. Iste vero atque quia explicabo porro nostrum itaque quod, voluptatibus, distinctio reiciendis, eaque omnis ab dolorum ducimus laudantium nisi suscipit. Obcaecati debitis ea eum nobis neque possimus quos repellat? Repellendus est, recusandae accusamus dignissimos necessitatibus aut molestias corporis laboriosam rerum atque veniam id. Reiciendis magni nulla possimus earum eos quas deleniti, architecto nisi autem, tenetur, fugiat quae aut minima natus quidem ullam! Soluta eaque maiores exercitationem ut adipisci, explicabo consequuntur voluptate magnam quas quaerat consectetur sed iste ipsa, provident odit vel voluptates voluptas quia veniam necessitatibus doloribus placeat nisi perferendis minus! Quod eaque tempore aperiam impedit ducimus inventore aut exercitationem non esse asperiores incidunt molestiae illo voluptatem, saepe assumenda totam dicta fugit nisi, beatae unde quae possimus delectus error. Iste delectus, doloribus debitis, quia tempora reprehenderit quidem rem nemo ab deserunt similique natus voluptates eveniet neque qui? Placeat iusto, minima nihil soluta magni quidem ratione officia debitis labore aliquam, animi, fugiat voluptatum similique saepe dignissimos! Maiores dolor est similique ad iure maxime, blanditiis temporibus vel fugiat vero voluptatibus impedit quae quam id rem alias dolore officiis ducimus laboriosam sunt unde nulla officia fuga. Placeat officiis est repudiandae voluptatibus at dignissimos molestias quis? Sit, nesciunt illum voluptate molestias magnam voluptas debitis commodi ex autem cupiditate explicabo?</p>
+      <div className="w-[100%] bg-[#f4ece6] resume-container p-12 relative">
+        <div className="content bg-white shadow-2xl border-r-10 min-h-[100vh] p-4 relative before:-content-[''] before:absolute before:top-0 before:left-0 before:w-[100px] before:h-[100px] before:border-l-4 before:border-t-4 before:border-blue-700 after:-content-[''] after:absolute after:bottom-0 after:right-0 after:w-[100px] after:h-[100px] after:border-r-4 after:border-b-4 after:border-blue-700">
+          <div className="header flex justify-center">
+            <h1 className="text-6xl relative text-gray-300 flex items-center uppercase after:content-[''] after:absolute after:-bottom-4 after:w-[100%] after:bg-blue-700">
+              <span className="w-[28px] h-[28px] mt-1 bg-blue-700 mr-2 block"></span>
+              <span>Resume</span>
+            </h1>
+          </div>
+          <div className="resume grid grid-cols-3 mt-20 gap-6">
+            <div className="summary bg-red-200 col-span-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+              sapiente ut illum voluptas, odit voluptatibus cum dolorum placeat
+              iusto sint fuga praesentium delectus architecto ipsum rerum iure,
+              officiis cupiditate? Magni expedita illo inventore saepe velit
+              quisquam consequuntur fuga neque quidem dolores suscipit officia a
+              tempora quis, fugit ipsum eum voluptas omnis quod enim earum
+              doloremque! Assumenda quisquam aliquid soluta quibusdam repellat!
+              Mollitia distinctio natus nostrum esse error qui officia corporis
+              perspiciatis deserunt ea fuga alias dolor illum, eum nisi, atque
+              quasi sit, veritatis architecto? A repellendus, odio corrupti quis
+              est eos veniam nisi iure non aliquam. Nam qui animi reprehenderit!
+            </div>
+            <div className="education bg-green-200">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+              sapiente ut illum voluptas, odit voluptatibus cum dolorum placeat
+              iusto sint fuga praesentium delectus architecto ipsum rerum iure,
+              officiis cupiditate? Magni expedita illo inventore saepe velit
+              quisquam consequuntur fuga neque quidem dolores suscipit officia a
+              tempora quis, fugit ipsum eum voluptas omnis quod enim earum
+              doloremque! Assumenda quisquam aliquid soluta quibusdam repellat!
+              Mollitia distinctio natus nostrum esse error qui officia corporis
+              perspiciatis deserunt ea fuga alias dolor illum, eum nisi, atque
+              quasi sit, veritatis architecto? A repellendus, odio corrupti quis
+              est eos veniam nisi iure non aliquam. Nam qui animi reprehenderit!
+            </div>
+            <div className="experience bg-blue-200">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
+              suscipit beatae commodi totam debitis dolore odio earum quo! Vero
+              ad fugit sunt, omnis rem itaque a magni beatae accusantium soluta
+              magnam consequuntur deleniti libero repudiandae ratione non at
+              suscipit! Omnis magnam harum impedit nobis. Ipsam cumque quae ex
+              aspernatur, provident nesciunt placeat nobis nostrum voluptatem!
+              Quod sequi qui, architecto ex accusamus labore saepe repellendus
+              tempora expedita asperiores quaerat esse libero pariatur omnis
+              aliquam alias illo, facere, excepturi laborum! Ducimus est iusto
+              consequatur, aperiam placeat nihil et, eos laboriosam nemo sit
+              quasi? Dolorum alias autem incidunt assumenda nam eos praesentium
+              consectetur.
+            </div>
+            <div className="skills bg-orange-200">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+              cum, sequi ipsam provident unde error ducimus minus voluptatibus
+              voluptate sunt aut porro accusamus ut reiciendis voluptas quam
+              nobis id repellat tempora consequuntur! Repellendus quisquam nemo
+              cum, neque quidem corrupti doloremque repellat ipsum ea. Quas
+              repellendus dolorem eaque nihil architecto tempore! Qui, similique
+              magnam maiores velit corporis sunt eius accusamus doloremque!
+              Inventore, accusantium voluptatibus incidunt amet repellendus quis
+              cupiditate aliquam dolore reprehenderit ratione facere quo, eaque
+              dicta. Voluptatibus animi blanditiis ad hic pariatur sunt corrupti
+              doloribus commodi, enim delectus, laudantium placeat quae
+              suscipit. Non omnis culpa illum exercitationem, distinctio
+              repellat praesentium?
+            </div>
+          </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
