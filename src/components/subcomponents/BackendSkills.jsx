bc29f4df4af89c1b80e0b6aec07a944815c1fd43
Mohd Rizwan <iamrizwan40@gmail.com>
2024-03-15 22:13:35 +0300

    reduced the padding of progress

--- src/components/subcomponents/BackendSkills.jsx
+++ src/components/subcomponents/BackendSkills.jsx
@@ -6,9 +6,9 @@ const BackendSkills = ({ skill }) => {
       <div className="skill flex flex-col gap-2">
         <h4>{skill.skill}</h4>
         <div className="content">
-          <div className="progress-bar w-[100%] p-[1px] rounded-md border-1 shadow-inner border-gray-300">
+          <div className="progress-bar w-[100%] p-[1px] rounded-md border-1 shadow-inner border-gray-600">
             <span
-              className={`progress block p-1 rounded-md bg-blue-800`}
+              className={`progress block p-[3px] rounded-md bg-blue-800`}
               style={{ width: `${skill.frequency}%` }}
             ></span>
           </div>
