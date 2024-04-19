9e41f49994614ccb2797545afbd3312a923693fa
Mohd Rizwan <iamrizwan40@gmail.com>
2024-04-19 21:05:06 +0300

    reduced the font-size of title and button

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,22 +9,22 @@ const Projects = () => {
     <>
       <div className="bg-gray-100 w-[100%]">
         <div className="projects bg-gray-100 relative md:w-[80%] m-auto md:py-8 p-4">
-          <div className="header mb-8 flex justify-between items-end">
+          <div className="header mb-8 max-md:mt-4 flex justify-between items-end">
             <div className="heading">
               <h3 className="flex items-center mb-2">
-                <span className="w-4 h-[2px] bg-blue-700 mr-2"></span>
-                <span className="md:text-xl text-sm text-gray-500">Projects</span>
+                <span className="md:w-4 w-2 h-[2px] bg-blue-700 md:mr-2 mr-[5px]"></span>
+                <span className="md:text-xl text-[12px] text-gray-500">Projects</span>
               </h3>
-              <h1 className="md:text-4xl text-2xl text-blue-700 font-[600]">
+              <h1 className="md:text-4xl text-xl text-blue-700 font-[600]">
                 My <spanc className="">Latest</spanc>{" "}
                 <span className="">Projects</span>
               </h1>
             </div>
-            <div className="view-all shadow-xl bg-transparent rounded-3xl flex gap-2 pl-[5px] items-center">
-              <span className="p-2 w-8 h-8 flex justify-center items-center bg-blue-700 rounded-full text-white">
-                <FontAwesomeIcon icon={faHandPointRight} />
+            <div className="view-all ring-blue-700 ring-1 shadow-xl bg-transparent rounded-3xl flex gap-2 pl-[5px] items-center">
+              <span className="p-2 w-8 h-8 max-sm:w-6 max-sm:h-6 max-sm:p-[5px] flex justify-center items-center bg-blue-700 rounded-full text-white">
+                <FontAwesomeIcon className="max-sm:text-[10px]" icon={faHandPointRight} />
               </span>
-              <Link className="bg-blue-700 text-white py-[10px] px-4 rounded-3xl text-sm" to={"/projects"}>View All Projects</Link>
+              <Link className="bg-blue-700 text-white py-[10px] max-sm:py-[5px] px-4 rounded-3xl text-sm max-sm:text-[10px]" to={"/projects"}>View All Projects</Link>
             </div>
           </div>
           {projects.map((project) => (
