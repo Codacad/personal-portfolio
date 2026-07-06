edb0a157e7cf378af77efaa132d2c14db80d06c1
Mohd Rizwan <iamrizwan40@gmail.com>
2026-07-06 16:20:42 +0300

    upgraded the dependencies to latest versions

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { NextUIProvider } from "@nextui-org/react";
 import { HashRouter, BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./state/store.js";
@@ -10,11 +9,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <NextUIProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </NextUIProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
